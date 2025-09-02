5b60b8ca6915cffbd2135bd4fdb8145a0109c14a
hammad2346 <hammadarif14244@gmail.com>
2025-09-02 19:03:49 +0500

    mobile side bar bug fix

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -17,7 +17,7 @@ const toggleSidebar=()=>{open.value=!open.value}
     <div v-if="!props.isMobile" 
            @mouseenter="toggleSidebar" 
            @mouseleave="toggleSidebar" 
-           class="h-[100%] rounded-[10px] bg-[#FBFCFE] z-10 flex flex-col shadow-lg gap-5 transition-all" 
+           class="h-[100%] rounded-[10px] bg-[#FBFCFE] flex flex-col shadow-lg gap-5 transition-all" 
            :class=sidebarsize >
 
 
@@ -26,24 +26,25 @@ const toggleSidebar=()=>{open.value=!open.value}
         <Conversations :open="open" />    
     </div>
 
-    <div v-else-if="props.isMobile && !open" class="fixed top-0 left-0 bg-[#FBFCFE] flex flex-col items-center p-5">
+    <div v-else-if="props.isMobile && !open" class="fixed top-0 left-0 z-50 flex flex-col items-center p-5">
       <button @click="toggleSidebar"><i class="pi pi-bars"></i></button>
     </div>
 
-    <div v-else-if="props.isMobile && open">
-      
-      <div @click="toggleSidebar" class="h-screen w-screen bg-black bg-opacity-50 z-10"></div>
-      
+    <div v-else-if="props.isMobile && open" class="fixed top-0 left-0 w-screen h-screen z-50 flex">
       <div 
-           
-           class="fixed top-0 left-0 h-[100%] w-[40%] rounded-r-[10px] z-20 bg-[#FBFCFE] flex flex-col shadow-lg gap-5 transition-all" 
-           >
+        @click="toggleSidebar" 
+        class="absolute top-0 left-0 w-full h-full bg-black bg-opacity-20"
+      ></div>
 
-
+      <div 
+        class="relative h-full w-[70%] sm:w-[40%] rounded-r-[10px] bg-[#FBFCFE] flex flex-col shadow-lg gap-5 transition-all"
+      >
         <sTopbar :open="open" />
         <Navbar :open="open" />
         <Conversations :open="open" />    
-    </div>
+      </div>
+
+      
     </div>
 
 
